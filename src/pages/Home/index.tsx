73ebcce3a3bc63a3805bbbb6f07c61ecb5fd7191
“jessica” <jessicagama9827@gmail.com>
2021-10-22 14:32:52 -0400

    --adicionando no carrinho

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -42,20 +42,20 @@ const Home = (): JSX.Element => {
   }, []);
 
   function handleAddProduct(id: number) {
-    // TODO
+    addProduct(id)
   }
 
   return (
     <ProductList>
       {products.map(product => (
-        <li>
+        <li key={product.id}>
         <img src={product.image} alt={product.title}/>
         <strong>{product.title}</strong>
         <span>{product.price}</span>
         <button
           type="button"
           data-testid="add-product-button"
-        // onClick={() => handleAddProduct(product.id)}
+          onClick={() => handleAddProduct(product.id)}
         >
           <div data-testid="cart-product-quantity">
             <MdAddShoppingCart size={16} color="#FFF" />
